9f700a6f8788fa7da759bc6bd3c69626037464af
shivamapchan <56571882+shivamapchan@users.noreply.github.com>
2022-11-01 16:18:58 -0400

    Update MainNavigation.js
    I have created a Link to the Profile page and added a little Icon for the Profile in the original reop.

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import React from "react";
 
 import classes from "./MainNavigation.module.css";
+import { CgProfile } from "react-icons/cg";
 
 const MainNavigation = () => {
 	return (
@@ -9,10 +10,19 @@ const MainNavigation = () => {
 			<Link to="/">
 				<div className={classes.logo}>Home</div>
 			</Link>
+
 			<nav>
 				<ul>
 					<li>
 						<Link to="/auth">Login</Link>
+					</li>
+					<li>
+						<CgProfile
+							className={classes.icon}
+						></CgProfile>
+						<Link to="/profile">
+							Profile
+						</Link>
 					</li>
 					<li>
 						<Link to="/">
