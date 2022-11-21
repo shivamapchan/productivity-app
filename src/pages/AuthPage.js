c163fb5e6bca47a031aae0fa1c2826ba42809a69
shivamapchan <56571882+shivamapchan@users.noreply.github.com>
2022-11-21 18:17:29 -0500

    Update and rename AuthPage.js to LoginPage.js

--- src/pages/AuthPage.js
+++
@@ -1,8 +0,0 @@
-import AuthForm from "../components/Auth/AuthForm";
-import React from "react";
-
-const AuthPage = () => {
-	return <AuthForm />;
-};
-
-export default AuthPage;
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,8 @@
+import LoginForm from "../components/Login/LoginForm";
+import React from "react";
+
+const LoginPage = () => {
+	return <LoginForm />;
+};
+
+export default LoginPage;
