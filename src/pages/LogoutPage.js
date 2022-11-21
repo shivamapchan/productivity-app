6ef1ad52f300cd092f157d0094d18d9efaed7c3f
shivamapchan <56571882+shivamapchan@users.noreply.github.com>
2022-11-21 18:24:34 -0500

    Create LogoutPage.js

---
+++ src/pages/LogoutPage.js
@@ -0,0 +1,8 @@
+import LogoutContent from "../components/Logout/LogoutContent";
+import React from "react";
+
+const LogoutPage = () => {
+	return <LogoutContent />;
+};
+
+export default LogoutPage;
