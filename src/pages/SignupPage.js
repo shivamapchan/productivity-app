fb8748de9012e4ea1a9cb944b343ebcfc6ea6682
shivamapchan <56571882+shivamapchan@users.noreply.github.com>
2022-11-21 18:29:46 -0500

    Create SignupPage.js

---
+++ src/pages/SignupPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import SignUpFormContent from "../components/Signup/SignUpForm";
+
+const SignupPage = () => {
+	return <SignUpFormContent />;
+};
+
+export default SignupPage;
