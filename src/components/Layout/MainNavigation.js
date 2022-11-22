468ada26593c862a62fa5ada970b701ff54f79ce
shivamapchan <56571882+shivamapchan@users.noreply.github.com>
2022-11-21 22:22:07 -0500

    Update MainNavigation.js

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,34 +1,74 @@
 import { Link } from "react-router-dom";
-import React from "react";
+import React, { useContext } from "react";
 
 import classes from "./MainNavigation.module.css";
 import { CgProfile } from "react-icons/cg";
+import AuthContext from "../../context/auth-context";
 
 const MainNavigation = () => {
+	const AuthCtx = useContext(AuthContext);
+	const isLoggedIn = AuthCtx.isLoggedIn;
+
+	const logoutHandler = () => {
+		AuthCtx.logout();
+	};
+
 	return (
 		<header className={classes.header}>
 			<Link to="/">
-				<div className={classes.logo}>Home</div>
+				<div className={classes.logo}>
+					Your To-Do Manager
+				</div>
 			</Link>
 
 			<nav>
 				<ul>
-					<li>
-						<Link to="/auth">Login</Link>
-					</li>
-					<li>
-						<CgProfile
-							className={classes.icon}
-						></CgProfile>
-						<Link to="/profile">
-							Profile
-						</Link>
-					</li>
-					<li>
-						<Link to="/">
-							<button>Logout</button>
-						</Link>
-					</li>
+					{!isLoggedIn && (
+						<li>
+							<Link to="/login">
+								Login
+							</Link>
+						</li>
+					)}
+					{!isLoggedIn && (
+						<li>
+							<Link to="/signup">
+								Sign Up
+							</Link>
+						</li>
+					)}
+					{isLoggedIn && (
+						<li>
+							<CgProfile
+								className={
+									classes.icon
+								}
+							></CgProfile>
+							<Link to="/profile">
+								Profile
+							</Link>
+						</li>
+					)}
+					{isLoggedIn && (
+						<li>
+							<Link to="/taskspage">
+								To-Do
+							</Link>
+						</li>
+					)}
+					{isLoggedIn && (
+						<li>
+							<Link to="/logout">
+								<button
+									onClick={
+										logoutHandler
+									}
+								>
+									Logout
+								</button>
+							</Link>
+						</li>
+					)}
 				</ul>
 			</nav>
 		</header>
